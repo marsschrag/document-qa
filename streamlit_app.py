f4fd340d56b3d72bdc68f90124ac993803acd9ac
marsukulele <mhschrag@syr.edu>
2026-01-26 18:35:56 +0000

    emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('IST 488 Labs')
-lab1 = st.Page('Labs/Lab1.py', title = 'Lab 1', icon = 'L')
-lab2 = st.Page('Labs/Lab2.py', title = 'Lab 2', icon = 'L')
+lab1 = st.Page('Labs/Lab1.py', title = 'Lab 1', icon = '⭐️')
+lab2 = st.Page('Labs/Lab2.py', title = 'Lab 2', icon = '⭐️')
 pg = st.navigation([lab2, lab1])
 st.set_page_config(page_title = 'IST 488 Labs',
                    initial_sidebar_state = 'expanded')
