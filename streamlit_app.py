e69b9a2ce87317987140c5a0f48ec48e9afa7da4
marsukulele <mhschrag@syr.edu>
2026-02-20 01:14:13 +0000

    lab5 get weather func

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ lab1 = st.Page('Labs/Lab1.py', title = 'Lab 1', icon = '⭐️')
 lab2 = st.Page('Labs/Lab2.py', title = 'Lab 2', icon = '🌈')
 lab3 = st.Page('Labs/Lab3.py', title = 'Lab 3', icon = '🍓')
 lab4 = st.Page('Labs/Lab4.py', title = 'Lab 4', icon = '🌼')
-pg = st.navigation([lab4, lab3, lab2, lab1])
+lab5 = st.Page('Labs/Lab5.py', title = 'Lab 5', icon = '✨')
+pg = st.navigation([lab5, lab4, lab3, lab2, lab1])
 st.set_page_config(page_title = 'IST 488 Labs',
                    initial_sidebar_state = 'expanded')
 pg.run()
 
 
-#emojis saved for later: 🍇🍄✨🐞🫧🥥🎨🪩💗
+#emojis saved for later: 🍇🍄🐞🫧🥥🎨🪩💗
